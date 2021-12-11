651d5a20a0a90fc92fceb991cca20480266fe156
Taraskiiin <taraskravets95@gmail.com>
2021-12-11 16:53:36 +0200

    feature(headed)Create header

--- src/Header/Header.tsx
+++ src/Header/Header.tsx
@@ -1,21 +1,32 @@
 import React from "react";
+
 import logo from "../assets/logo.png";
+import { SearchIcon } from "../assets/icons";
+
+import "./header.scss";
+
 const Header = () => {
   return (
-    <header>
-      <a href="#">
-        <img width={200} height={200} src={logo} alt="logo" />
-        <span className="header_text">STATISTIC</span>
-      </a>
-      <label>
-        <input
-          type="search"
-          id="site-search"
-          name="q"
-          aria-label="Search through site content"
-          placeholder="Search..."
-        ></input>
-      </label>
+    <header className="header">
+      <div className="wrapper">
+        <div className="header-block">
+          <a href="#" className="header-link">
+            <img width={200} height={200} src={logo} alt="logo" />
+            <span className="link_text">STATISTIC</span>
+          </a>
+          <label className="search-label">
+            <input
+              className="search-input"
+              type="search"
+              id="site-search"
+              name="q"
+              aria-label="Search through site content"
+              placeholder="Search..."
+            />
+            <SearchIcon />
+          </label>
+        </div>
+      </div>
     </header>
   );
 };
--- src/index.tsx
+++ src/index.tsx
@@ -1,7 +1,7 @@
 import React from "react";
 import ReactDOM from "react-dom";
-import ".styles/reset.scss";
-import ".styles/index.scss";
+import "./styles/reset.scss";
+import "./styles/styles.scss";
 import App from "./App";
 
 ReactDOM.render(
